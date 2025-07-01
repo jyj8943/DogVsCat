5c11e7884409b7a0a4fb15187705ed68c2023fda
jyj8943 <jyj8943@gmail.com>
2025-07-01 16:32:06 +0900

    3-2 강아지 만들기
    밥 쏘기

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    public GameObject food;
+    
+    void Start()
+    {
+        InvokeRepeating("MakeFood", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float x = mousePos.x;
+
+        if (x > 8.5f)
+        {
+            x = 8.5f;
+        }
+        if (x < -8.5f)
+        {
+            x = -8.5f;
+        }
+        
+        transform.position = new Vector2(x, transform.position.y);
+    }
+
+    private void MakeFood()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y + 2f;
+        
+        Instantiate(food, new Vector2(x, y), quaternion.identity);
+    }
+}
